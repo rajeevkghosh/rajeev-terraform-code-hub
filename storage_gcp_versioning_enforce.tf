1f96f63155b2a5a9f588f358bf9cd46e0de25f1b
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-11-17 14:19:52 +0530

    first push

---
+++ storage_gcp_versioning_enforce.tf
@@ -0,0 +1,44 @@
+provider "google" {
+ access_token = var.access_token
+}
+
+resource "google_storage_bucket" "rockstar" {
+  name          = "rockstar-bucket"
+  project = "airline1-sabre-wolverine"
+  location      = "US"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    condition {
+      age = 3
+      num_newer_versions = 5
+    }
+    action {
+      type = "Delete"
+    }
+
+  }
+
+}
+
+
+resource "google_storage_bucket" "hellobucket" {
+  name          = "hello-bucket-air"
+  project = "airline1-sabre-wolverine"
+  location      = "US"
+  force_destroy = true
+  
+  lifecycle_rule {
+    condition {
+      age = 3
+
+    }
+    action {
+      type = "Delete"
+    }
+
+  }
+
+}
