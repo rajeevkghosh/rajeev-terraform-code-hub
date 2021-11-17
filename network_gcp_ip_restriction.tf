d3bb9b4eb30df2f5dc1ecdb8a1243060f09d09b9
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-11-17 20:30:57 +0530

    pushed code

--- network_gcp_ip_restriction.tf
+++ network_gcp_ip_restriction.tf
@@ -5,7 +5,7 @@ resource "google_dataproc_cluster" "simplecluster" {
   cluster_config {
     gce_cluster_config {
       zone = "us-central1-a"
-      internel_ip_only = true
+      internal_ip_only = true
 
       # One of the below to hook into a custom network / subnetwork
       network    = "default"
